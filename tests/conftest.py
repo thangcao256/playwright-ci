cffa5c7ba7aae98bd5f03132d9fcbfb7d3cfa770
Thang Cao <thangminhcao2000@gmail.com>
2025-07-02 21:39:13 +0700

    headless true update

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,6 @@ import pytest
 def browser_type_launch_args(browser_type_launch_args):
     return {
         **browser_type_launch_args,
-        "headless": False,
+        "headless": True,
         "slow_mo": 500
     }
